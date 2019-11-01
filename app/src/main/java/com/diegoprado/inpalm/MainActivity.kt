bceec735a5711073b3ddc11c765e0ed59b42e283
diegoprado-developer <diego.droiddeveloper@gmail.com>
2019-10-31 21:47:42 -0300

    adicionando biblioteca de slide

--- app/src/main/java/com/diegoprado/inpalm/MainActivity.kt
+++ app/src/main/java/com/diegoprado/inpalm/MainActivity.kt
@@ -2,11 +2,14 @@ package com.diegoprado.inpalm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.heinrichreimersoftware.materialintro.app.IntroActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : IntroActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+//        setContentView(R.layout.activity_main)
+
+
     }
 }

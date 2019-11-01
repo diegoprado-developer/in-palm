7bcdc37aa38067d0ce1334df84ec69d645163a67
diegoprado-developer <diego.droiddeveloper@gmail.com>
2019-10-31 23:34:59 -0300

    estilo dos slides

--- app/src/main/java/com/diegoprado/inpalm/MainActivity.kt
+++ app/src/main/java/com/diegoprado/inpalm/MainActivity.kt
@@ -1,8 +1,8 @@
 package com.diegoprado.inpalm
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.heinrichreimersoftware.materialintro.app.IntroActivity
+import com.heinrichreimersoftware.materialintro.slide.FragmentSlide
 
 class MainActivity : IntroActivity() {
 
@@ -10,6 +10,36 @@ class MainActivity : IntroActivity() {
         super.onCreate(savedInstanceState)
 //        setContentView(R.layout.activity_main)
 
+        isButtonBackVisible.not()
+        isButtonNextVisible.not()
+
+        addSlide(
+            FragmentSlide.Builder()
+            .background(android.R.color.holo_orange_dark)
+            .fragment(R.layout.slider_1)
+            .build()
+        )
+
+        addSlide(
+            FragmentSlide.Builder()
+                .background(android.R.color.white)
+                .fragment(R.layout.slider_2)
+                .build()
+        )
+
+        addSlide(
+            FragmentSlide.Builder()
+                .background(android.R.color.white)
+                .fragment(R.layout.slider_3)
+                .build()
+        )
+
+        addSlide(
+            FragmentSlide.Builder()
+                .background(android.R.color.white)
+                .fragment(R.layout.slider_4)
+                .build()
+        )
 
     }
 }
